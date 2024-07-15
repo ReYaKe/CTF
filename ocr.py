ecbd37938aad18c43af6e44aeab37bd747d59ad2
hiyosilver <130156584+hiyosilver@users.noreply.github.com>
2024-07-15 11:02:44 +0200

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,35 +1,29 @@
 import pyautogui
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageOps
 import pyperclip
 
 #zoom240
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 # Example: Capture a 400x300 region starting from (0, 0)
-screenshot_region = pyautogui.screenshot(region=(50, 750, 1350, 90))
-screenshot_region.save("screenshot_region.png")
-# Open the color image
-image_file = Image.open("screenshot_region.png")
+screenshot_image: Image = pyautogui.screenshot(region=(60, 780, 1450, 120))
+# screenshot_region = pyautogui.screenshot(region=(50, 750, 1350, 90))
 
 # Convert the image to black and white (grayscale)
-bw_image = image_file.convert("L")
+bw_image: Image = screenshot_image.convert("L")
 
-# Save the black and white image
-bw_image.save("bw_image.png")
+post_image: Image = ImageOps.posterize(bw_image, 2)
 
-# Load the screenshot image
-screenshot = Image.open('bw_image.png')
+# Save the posterized image
+post_image.save("post_image.png")
 
 # Extract text using OCR
-extracted_text = pytesseract.image_to_string(screenshot)
+extracted_text = pytesseract.image_to_string(post_image)[::-1]
 stripped_text = extracted_text.replace(" ", "")
 stripped_text = stripped_text.replace("0", "O")
 stripped_text = stripped_text.replace("1", "l")
 
-
-reversed_text = stripped_text[::-1]
 print("Reversed text:")
-print(reversed_text)
-pyperclip.copy(reversed_text)
-
+print(stripped_text)
+pyperclip.copy(stripped_text)
